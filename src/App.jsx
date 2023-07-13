e2fcba0abddd8dbc7b0abb6fccf5e743d6eaf0b3
alejoevilches <alejoevilches@gmail.com>
2023-07-13 00:00:24 -0300

    start logic for photo fetch

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,29 @@ import React, { useEffect, useState } from 'react'
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 
 export function App () {
-  const [fact, setFact] = useState('Los gatos son buenos')
+  const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then(res => res.json())
-      .then(data => setFact(data.fact))
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
+        const firstWord = (fact.split(' ')[0])
+
+        fetch(`https://cataas.com/c/s/${firstWord}?json=true`)
+          .then(res => res.json())
+          .then(data => {
+            const { url } = data
+            setImageUrl(url)
+          })
+      })
   }, [])
   return (
     <>
       <h1>App de Gatos</h1>
-      <p>{fact}</p>
+      {fact && <p>{fact}</p>}
+      {imageUrl && <img url={imageUrl} alt='A photo of a random cat provided by the Cataas API' />}
     </>
   )
 }
